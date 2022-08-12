0ba96d13115dc633aa9f5445cf47f9e1e2a34d25
Joseph Emmanuel <joseph.emmanuel@greatgulf.com>
2022-08-12 04:37:23 -0400

    initial commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3801521da7a1cc55752f5f03fa84835c'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7da04ac1-8a1a-4505-8812-5b630426609b'
+};
