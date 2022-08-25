62df5fd4ed9e65f2f1fceb6478db644e7f14383a
Joseph Emmanuel <joseph.emmanuel@greatgulf.com>
2022-08-25 14:45:17 -0400

    added static page for v3

---
+++ v3/client static/components/shared/BlockManager/index.js
@@ -0,0 +1,31 @@
+import Hero from "../../blocks/Hero";
+import Pricing from "../../blocks/Pricing";
+import CtaCommandLine from "../../blocks/CtaCommandLine";
+
+const getBlockComponent = ({ __component, ...rest }, index) => {
+  let Block;
+
+  switch (__component) {
+    case "blocks.hero":
+      Block = Hero;
+      break;
+    case "blocks.pricing":
+      Block = Pricing;
+      break;
+    case "blocks.cta-command-line":
+      Block = CtaCommandLine;
+      break;
+  }
+
+  return Block ? <Block key={`index-${index}`} {...rest} /> : null;
+};
+
+const BlockManager = ({ blocks }) => {
+  return <div>{blocks.map(getBlockComponent)}</div>;
+};
+
+BlockManager.defaultProps = {
+  blocks: [],
+};
+
+export default BlockManager;
---
+++ v3/client static/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+// module.exports = nextConfig
+module.exports = {
+  trailingSlash: true,
+  images: {
+    loader: "imgix",
+    path: "",
+  },
+};
